460a35bda830c0ca13cfb3ca85696ddad11fdf43
Rally <crazyfoxw@hotmail.com>
2016-08-05 17:32:59 +0800

    Set  DIS Open Data Cloud as Startup project in the VS solution; Controlers and Views added for Business Management, Storage Management, Service Management in DIS Open Data Cloud;

---
+++ DIS-Open.Org/DISOpenDataCloud/Controllers/BusinessController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DISOpenDataCloud.Controllers
+{
+    public class BusinessController : Controller
+    {
+        // GET: Business
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Business/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Business/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Business/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Business/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Business/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Business/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Business/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DIS-Open.Org/DISOpenDataCloud/Controllers/DatabaseController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DISOpenDataCloud.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        // GET: Database
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Database/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Database/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Database/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Database/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Database/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Database/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Database/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
