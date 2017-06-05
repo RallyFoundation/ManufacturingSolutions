c02179278b35e5af2d761354987e33e050d825e4
Rally Wang <v-rawang@microsoft.com>
2017-06-05 14:46:31 +0800

    Facade for caching added.

--- ManufacturingPlatform/Platform.DAAS.OData.Facade/Provider.cs
+++ ManufacturingPlatform/Platform.DAAS.OData.Facade/Provider.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Concurrent;
 using Platform.DAAS.OData.Core.HTTP;
 using Platform.DAAS.OData.Core.Logging;
 using Platform.DAAS.OData.Core.Security;
 using Platform.DAAS.OData.Core.ServiceManagement;
 using Platform.DAAS.OData.Core.BusinessManagement;
 using Platform.DAAS.OData.Core.StorageManagement;
+using Platform.DAAS.OData.Core.Caching;
 using Platform.DAAS.OData.Authentication;
 using Platform.DAAS.OData.HTTP;
 using Platform.DAAS.OData.Logging;
@@ -17,11 +20,94 @@ using Platform.DAAS.OData.Security;
 using Platform.DAAS.OData.ServiceManager;
 using Platform.DAAS.OData.BusinessManagement;
 using Platform.DAAS.OData.StorageManagement;
+using Platform.DAAS.OData.Caching;
 
 namespace Platform.DAAS.OData.Facade
 {
     public class Provider
     {
+        static ConcurrentDictionary<string, object> Managers = new ConcurrentDictionary<string, object>();
+
+        public static ICacheManager GetCacheManager()
+        {
+            ICacheManager manager = (Managers.ContainsKey("CacheManager") && Managers["CacheManager"] != null) ? (Managers["CacheManager"] as ICacheManager) : null;
+
+            if (manager == null || manager.HadExceptions())
+            {
+                manager = new CacheManagerRedis(ConfigurationManager.AppSettings.Get("CacheServerAddress"), int.Parse(ConfigurationManager.AppSettings.Get("CacheServerPort")), ConfigurationManager.AppSettings.Get("CacheServerPassword"), long.Parse(ConfigurationManager.AppSettings.Get("CacheServerDBIndex")));
+
+                //manager = new CacheManagerMemcached();
+
+                if (!Managers.ContainsKey("CacheManager"))
+                {
+                    Managers.TryAdd("CacheManager", manager);
+                }
+                else
+                {
+                    Managers["CacheManager"] = manager;
+                }
+            }
+
+            return manager;
+        }
+
+        internal static void SetCacheManager()
+        {
+            ICacheManager manager = new CacheManagerRedis(ConfigurationManager.AppSettings.Get("CacheServerAddress"), int.Parse(ConfigurationManager.AppSettings.Get("CacheServerPort")), ConfigurationManager.AppSettings.Get("CacheServerPassword"), long.Parse(ConfigurationManager.AppSettings.Get("CacheServerDBIndex"))); //new CacheManagerMemcached(); 
+
+            if (!Managers.ContainsKey("CacheManager"))
+            {
+                Managers.TryAdd("CacheManager", manager);
+            }
+            else
+            {
+                Managers["CacheManager"] = manager;
+            }
+        }
+
+
+        public static ICacheManager GetWdsCacheManager()
+        {
+            ICacheManager manager = (Managers.ContainsKey("WdsCacheManager") && Managers["WdsCacheManager"] != null) ? (Managers["WdsCacheManager"] as ICacheManager) : null;
+
+            if (manager == null || manager.HadExceptions())
+            {
+                manager = new CacheManagerRedis(ConfigurationManager.AppSettings.Get("WdsCacheServerAddress"), int.Parse(ConfigurationManager.AppSettings.Get("WdsCacheServerPort")), ConfigurationManager.AppSettings.Get("WdsCacheServerPassword"), long.Parse(ConfigurationManager.AppSettings.Get("WdsCacheServerDBIndex")));
+
+                //manager = new CacheManagerMemcached();
+
+                if (!Managers.ContainsKey("WdsCacheManager"))
+                {
+                    Managers.TryAdd("WdsCacheManager", manager);
+                }
+                else
+                {
+                    Managers["WdsCacheManager"] = manager;
+                }
+            }
+
+            return manager;
+        }
+
+        internal static void SetWdsCacheManager()
+        {
+            ICacheManager manager = new CacheManagerRedis(ConfigurationManager.AppSettings.Get("WdsCacheServerAddress"), int.Parse(ConfigurationManager.AppSettings.Get("WdsCacheServerPort")), ConfigurationManager.AppSettings.Get("WdsCacheServerPassword"), long.Parse(ConfigurationManager.AppSettings.Get("WdsCacheServerDBIndex"))); //new CacheManagerMemcached(); 
+
+            if (!Managers.ContainsKey("WdsCacheManager"))
+            {
+                Managers.TryAdd("WdsCacheManager", manager);
+            }
+            else
+            {
+                Managers["WdsCacheManager"] = manager;
+            }
+        }
+
+        internal static void ResetManagers()
+        {
+            Managers = new ConcurrentDictionary<string, object>();
+        }
+
         public static IResourceRouter ResourceRouter()
         {
             return new ResourceRouter(new ExceptionHandler());
