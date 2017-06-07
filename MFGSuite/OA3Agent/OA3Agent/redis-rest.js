71d10fe61bfdd985225aa65c1ace99e720fb8ffe
Rally Wang <v-rawang@microsoft.com>
2017-06-07 11:08:30 +0800

    Redis RESTful service added.

---
+++ MFGSuite/OA3Agent/OA3Agent/redis-rest.js
@@ -0,0 +1,58 @@
+﻿var app = require('express')();
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+var redis = require("redis");
+var redisClient = null;
+
+var redisAddress = "127.0.0.1";
+var redisPort = 6379;
+var redisPassword = "P@ssword1";
+var redisDbIndex = 0;
+
+var httpServerPort = 8087;
+
+function getRedisClient() {
+    var client = redis.createClient(redisPort, redisAddress);
+    client.auth(redisPassword);
+
+    return client;
+}
+
+var server = app.listen(httpServerPort, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Redis RESTful API service listening at http://%s:%s", host, port)
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello!');
+});
+
+app.get('/redis/:key', function (req, res)
+{
+    var redisClient = getRedisClient();
+    var key = req.params.key;
+
+    redisClient.select(redisDbIndex, function (err)
+    {
+        if (err){
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            redisClient.get(key, function (err, result)
+            {
+                if (err) {
+                    console.log(err);
+                    res.end(err);
+                }
+                else
+                {
+                    console.log(result);
+                    res.end(result);
+                }
+            });
+        }
+    })
+});
