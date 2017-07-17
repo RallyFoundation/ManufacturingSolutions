741c6ba7e6e2dce95ce2126c77342db94be20991
Rally Wang <v-rawang@microsoft.com>
2017-07-17 16:49:25 +0800

    Data Computing PowerShell module provided, with F23.StringSimilarity NuGet Package.

---
+++ ManufacturingPlatform/PowerShellDataComputing/GetJaroWinklerSimilarityCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using F23.StringSimilarity;
+
+namespace PowerShellDataComputing
+{
+    [Cmdlet(VerbsCommon.Get, "JaroWinklerSimilarity")]
+    public class GetJaroWinklerSimilarityCommand : Cmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "The string to be compared.")]
+        public string ComparedString { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true, HelpMessage = "The string to be compared with.")]
+        public string ComparingString { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            //base.ProcessRecord();
+
+            JaroWinkler jaroWinkler = new JaroWinkler();
+
+            double similarity = jaroWinkler.Similarity(ComparedString, ComparingString);
+
+            this.WriteObject(similarity);
+        }
+    }
+}
---
+++ ManufacturingPlatform/PowerShellDataComputing/GetLevenshteinDistanceCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using F23.StringSimilarity;
+
+namespace PowerShellDataComputing
+{
+    [Cmdlet(VerbsCommon.Get, "LevenshteinDistance")]
+    public class GetLevenshteinDistanceCommand : Cmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true, HelpMessage = "The string to be compared.")]
+        public string ComparedString { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true, HelpMessage = "The string to be compared with.")]
+        public string ComparingString { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            //base.ProcessRecord();
+
+            Levenshtein levenshtein = new Levenshtein();
+
+            double distance = levenshtein.Distance(ComparedString, ComparingString);
+
+            this.WriteObject(distance);
+        }
+    }
+}
