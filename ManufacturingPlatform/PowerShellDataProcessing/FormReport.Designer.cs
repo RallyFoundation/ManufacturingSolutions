0a7e7a5d8a42964b49a545d85d4e9d955643c1b1
Rally Wang <v-rawang@microsoft.com>
2017-07-18 15:11:21 +0800

    ShowReport Cmdlet provided in Data Processing PS Module.

---
+++ ManufacturingPlatform/PowerShellDataProcessing/FormReport.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace PowerShellDataProcessing
+{
+    partial class FormReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowserReport = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowserReport
+            // 
+            this.webBrowserReport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowserReport.Location = new System.Drawing.Point(20, 60);
+            this.webBrowserReport.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowserReport.Name = "webBrowserReport";
+            this.webBrowserReport.Size = new System.Drawing.Size(760, 520);
+            this.webBrowserReport.TabIndex = 0;
+            this.webBrowserReport.FileDownload += new System.EventHandler(this.webBrowserReport_FileDownload);
+            this.webBrowserReport.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowserReport_Navigating);
+            // 
+            // FormReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.webBrowserReport);
+            this.Name = "FormReport";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowserReport;
+    }
+}
---
+++ ManufacturingPlatform/PowerShellDataProcessing/FormReport.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace PowerShellDataProcessing
+{
+    public partial class FormReport : MetroForm
+    {
+        public FormReport()
+        {
+            InitializeComponent();
+        }
+
+        public string Uri { get; set; }
+
+        public void Navigate(string uri)
+        {
+            this.Uri = uri;
+            //this.webBrowserReport.Url = new Uri(uri);
+            this.webBrowserReport.Navigate(uri);
+            this.webBrowserReport.Refresh();
+        }
+
+        private void webBrowserReport_FileDownload(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowserReport_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            
+        }
+    }
+}
