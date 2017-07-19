0c24918850239973ecf628fc4c11fc966fadcf62
Rally Wang <v-rawang@microsoft.com>
2017-07-19 20:50:04 +0800

    Cross Domain Access (CORS) for WDS API added; File API provided.

--- MFGSuite/OA3Agent/OA3Agent/wds-api.js
+++ MFGSuite/OA3Agent/OA3Agent/wds-api.js
@@ -3,10 +3,21 @@ var bodyParser = require('body-parser');
 var fs = require("fs");
 var shell = require('node-powershell');
 var redis = require("redis");
+var cors = require("cors");
 
 var app = express();
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+//for cross domain access
+//app.all('*', function (req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header("Access-Control-Allow-Headers", "X-Requested-With,Authorization,Content-Type");
+//    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//    res.header("Content-Type", "application/json;charset=utf-8");
+//    next();
+//});
+app.use(cors());
 
 var httpServerPort = 8089;
 
