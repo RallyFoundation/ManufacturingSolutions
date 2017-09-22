2883c8d707e97c62e703e6846fe2dd61bb3306ca
Rally Wang <v-rawang@microsoft.com>
2017-09-22 14:27:31 +0800

    Mongo DB persistency for client status added; Delete for install image added.

--- MFGSuite/OA3Agent/OA3Agent/wds-api.js
+++ MFGSuite/OA3Agent/OA3Agent/wds-api.js
@@ -7,6 +7,8 @@ var shell = require('node-powershell');
 var redis = require("redis");
 var cors = require("cors");
 var config = require("nodejs-config")(__dirname);
+var mongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 //var bearerToken = require('express-bearer-token');
 //var async = require("async");
 
@@ -38,6 +40,8 @@ var redisPort = config.get("app.redis-port"); //6379;
 var redisPassword = config.get("app.redis-password"); //"P@ssword1";
 var redisDbIndex = config.get("app.redis-db-index"); //0;
 var redisDbIndexClientStatus = config.get("app.redis-db-index-client-status"); //0;
+var mongoDBUrl = config.get("app.mongodb-url");;
+var mongoDBCollectionName = config.get("app.mongodb-collection-name");;
 var installImageRepository = config.get("app.install-image-repository");
 var bootImageRepository = config.get("app.boot-image-repository");
 var ffuImageRepository = config.get("app.ffu-image-repository");
@@ -100,7 +104,7 @@ io.on("connection", function (socket) {
     console.log("A new app connected!");
 
     socket.on("msg:newdata", function (data) {
-        persistData(data);
+        //persistData(data);
         //io.emit("msg:msgrly", data);
         //io.emit("msg:resp", "OK! " + Date.now().toString());
     });
@@ -127,8 +131,35 @@ function getRedisClient() {
     return client;
 }
 
-function persistData(data)
-{
+var insertDocuments = function (db, data, callback) {
+    var collection = db.collection(mongoDBCollectionName);
+    collection.insertOne(
+        data,
+        function (err, result) {
+            assert.equal(err, null);
+            assert.equal(1, result.result.n);
+            assert.equal(1, result.ops.length);
+            console.log("OK!");
+            callback(result);
+        });
+}
+
+function persistData(data) {
+    mongoClient.connect(mongoDBUrl, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected to MongoDB.");
+
+        insertDocuments(db, data, function (res) {
+            db.close();
+
+            console.log(res);
+
+            data.id = res.insertedId;
+            console.log(JSON.stringify(data));
+
+            //io.emit("msg:msgrly", data);
+        });
+    });
 }
 
 //function updateClientStatus(data)
@@ -290,6 +321,8 @@ app.post('/wds/terminal/status/', function (req, res) {
     var redisClient = getRedisClient();
     //var key = req.params.key;
     var data = req.body;
+
+    persistData(data);
 
     redisClient.select(redisDbIndexClientStatus, function (err) {
         if (err) {
@@ -508,7 +541,8 @@ app.get("/wds/imagefile/install/:fname", function (req, res) {
         console.log(JSON.stringify(stats));
 
         if (err) {
-            res.end(err.message);
+            console.log(err);
+            res.end(err);
         }
 
         res.setHeader("Content-Length", stats.size);
@@ -532,7 +566,8 @@ app.get("/wds/imagefile/boot/:fname", function (req, res) {
         console.log(JSON.stringify(stats));
 
         if (err) {
-            res.end(err.message);
+            console.log(err);
+            res.end(err);
         }
 
         res.setHeader("Content-Length", stats.size);
@@ -556,7 +591,8 @@ app.get("/wds/imagefile/ffu/:fname", function (req, res) {
         console.log(JSON.stringify(stats));
 
         if (err) {
-            res.end(err.message);
+            console.log(err);
+            res.end(err);
         }
 
         res.setHeader("Content-Length", stats.size);
@@ -584,7 +620,8 @@ app.get("/wds/imagefile/info/install/:fname", function (req, res) {
         res.end(info);
 
         if (err) {
-            res.end(err.message);
+            console.log(err);
+            res.end(err);
         }
     });
 }); 
@@ -601,7 +638,8 @@ app.get("/wds/imagefile/info/boot/:fname", function (req, res) {
         res.end(info);
 
         if (err) {
-            res.end(err.message);
+            console.log(err);
+            res.end(err);
         }
     });
 });
@@ -618,7 +656,8 @@ app.get("/wds/imagefile/info/ffu/:fname", function (req, res) {
         res.end(info);
 
         if (err) {
-            res.end(err.message);
+            console.log(err);
+            res.end(err);
         }
     });
 });
@@ -982,7 +1021,7 @@ app.post('/wds/image/content/', function (req, res) {
             //ps.dispose();
             res.end(err);
         });
-})
+});
 
 app.post('/wds/imagegroup/install/', function (req, res) {
     var data = req.body;
@@ -1024,10 +1063,9 @@ app.post('/wds/imagegroup/install/', function (req, res) {
             //ps.dispose();
             res.end(err);
         });
-})
+});
 
-app.post('/wds/image/install/', function (req, res)
-{
+app.post('/wds/image/install/', function (req, res) {
     var data = req.body;
 
     let ps = new shell({
@@ -1040,12 +1078,10 @@ app.post('/wds/image/install/', function (req, res)
         .then(output => {
             console.log(output);
             //res.end(output);
-            if (data.EnableMulticastTransmission)
-            {
+            if (data.EnableMulticastTransmission) {
                 ps.addCommand('$image = Import-WdsInstallImage', ['ImageGroup "' + data.ImageGroupName + '"', 'Path "' + data.ImageFilePath + '"', 'NewImageName "' + data.NewImageName + '"', 'NewDescription "' + data.NewDescription + '"', 'NewFileName "' + data.NewFileName + '"', 'ImageName "' + data.RawImageNameInFile + '"', 'Multicast', 'TransmissionName "' + data.MulticastTransmissionName + '"']);
             }
-            else
-            {
+            else {
                 ps.addCommand('$image = Import-WdsInstallImage', ['ImageGroup "' + data.ImageGroupName + '"', 'Path "' + data.ImageFilePath + '"', 'NewImageName "' + data.NewImageName + '"', 'NewDescription "' + data.NewDescription + '"', 'NewFileName "' + data.NewFileName + '"', 'ImageName "' + data.RawImageNameInFile + '"']);
             }
             ps.invoke()
@@ -1074,7 +1110,7 @@ app.post('/wds/image/install/', function (req, res)
             //ps.dispose();
             res.end(err);
         });
-})
+});
 
 
 app.post('/wds/image/boot/', function (req, res) {
@@ -1122,7 +1158,7 @@ app.post('/wds/image/boot/', function (req, res) {
             //ps.dispose();
             res.end(err);
         });
-})
+});
 
 
 app.patch('/wds/image/boot/', function (req, res) {
@@ -1170,7 +1206,7 @@ app.patch('/wds/image/boot/', function (req, res) {
             //ps.dispose();
             res.end(err);
         });
-})
+});
 
 
 app.delete('/wds/image/boot/:arch/:name', function (req, res) {
@@ -1196,8 +1232,7 @@ app.delete('/wds/image/boot/:arch/:name', function (req, res) {
             //res.end(output);
             if (fileName != null && fileName != '') {
                 ps.addCommand('$image = Remove-WdsBootImage', ['Architecture ' + architecture, 'ImageName "' + imageName + '"', 'FileName "' + fileName + '"']);
-            } else
-            {
+            } else {
                 ps.addCommand('$image = Remove-WdsBootImage', ['Architecture ' + architecture, 'ImageName "' + imageName + '"']);
             }
             ps.invoke()
@@ -1226,4 +1261,59 @@ app.delete('/wds/image/boot/:arch/:name', function (req, res) {
             //ps.dispose();
             res.end(err);
         });
-})
+});
+
+
+app.delete('/wds/image/install/:group/:name', function (req, res) {
+    //var data = req.body;
+
+    var imageName = req.params.name;
+    imageName = String(imageName).replace(/\$/g, " ");
+
+    var imageGroup = req.params.group;
+
+    var fileName = req.query.fname != null ? String(req.query.fname).replace(/\$/g, " ") : null;
+    //fileName = String(fileName).replace(/\$/g, " ");
+
+    let ps = new shell({
+        executionPolicy: 'Bypass',
+        noProfile: true
+    });
+
+    ps.addCommand('Import-Module', ['Name "WDS"']);
+    ps.invoke()
+        .then(output => {
+            console.log(output);
+            //res.end(output);
+            if (fileName != null && fileName != '') {
+                ps.addCommand('$image = Remove-WdsInstallImage', ['InstallImageName "' + imageName + '"', 'ImageGroup "' + imageGroup + '"', 'FileName "' + fileName + '"']);
+            } else {
+                ps.addCommand('$image = Remove-WdsInstallImage', ['InstallImageName "' + imageName + '"', 'ImageGroup "' + imageGroup + '"']);
+            }
+            ps.invoke()
+                .then(output => {
+                    console.log(output);
+                    //res.end(output);
+                    ps.addCommand('$image | ConvertTo-Json')
+                    ps.invoke()
+                        .then(output => {
+                            console.log(output);
+                            res.end(output);
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            ps.dispose();
+                        });
+                })
+                .catch(err => {
+                    console.log(err);
+                    //ps.dispose();
+                    res.end(err);
+                });
+        })
+        .catch(err => {
+            console.log(err);
+            //ps.dispose();
+            res.end(err);
+        });
+});
