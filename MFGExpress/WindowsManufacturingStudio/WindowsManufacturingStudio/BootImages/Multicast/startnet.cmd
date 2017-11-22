86e80bd92c1b2591ab88d987b72129c878de0520
Rally Wang <v-rawang@microsoft.com>
2017-11-22 16:33:30 +0800

    powercfg added in the winpe startnet for creating the boot pe each for ffu and wds multicast.

--- MFGExpress/WindowsManufacturingStudio/WindowsManufacturingStudio/BootImages/Multicast/startnet.cmd
+++ MFGExpress/WindowsManufacturingStudio/WindowsManufacturingStudio/BootImages/Multicast/startnet.cmd
@@ -1,4 +1,8 @@
 wpeinit
+
+call powercfg /list
+call powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+
 diskpart /s %WINDIR%\System32\diskpartcmd.txt
 :rem==%WINDIR%\System32\Wdsmcast\wdsmcast.exe /progress /verbose /trace:wds_trace.etl /Apply-Image /Server:192.168.0.215 /Namespace:WDS:Group-Windows8/Win8-Windows.wim/1 /Username:WIN-Server-02\Administrator /Password:P@ssword! /SourceFile:Win8-Windows.wim /Index:1 /DestinationPath:W:\
 :rem==%WINDIR%\System32\Wdsmcast\wdsmcast.exe /progress /verbose /trace:wds_trace.etl /Transfer-File /Server:192.168.0.215 /Namespace:WDS:Group-Windows8/Win8-Windows.wim/1 /Username:WIN-Server-02\Administrator /Password:P@ssword! /SourceFile:Win8-Windows.wim /DestinationFile:R:\install.wim
