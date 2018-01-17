47f218b452401e170173b43dfe51464d464a4ecd
Rally Wang <v-rawang@microsoft.com>
2018-01-17 13:35:34 +0800

    Content display adjusted.

--- WindowsManufacturingCloud/WindowsManufacturingCloud/gulpfile.js
+++ WindowsManufacturingCloud/WindowsManufacturingCloud/gulpfile.js
@@ -16,11 +16,11 @@ gulp.task('min:js', function () {
         .pipe(gulp.dest('wwwroot/js'));
 });
 
-//gulp.task('min:cfg', function () {
-//    // place code for your default task here
-//    return gulp.src('Configs/config.json')
-//        .pipe(gulp.dest('wwwroot/js'));
-//});
+gulp.task('min:css', function () {
+    // place code for your default task here
+    return gulp.src('Contents/*.*')
+        .pipe(gulp.dest('wwwroot/asset'));
+});
 
 
-gulp.task("min", ["min:js"]);
+gulp.task("min", ["min:js", "min:css"]);
