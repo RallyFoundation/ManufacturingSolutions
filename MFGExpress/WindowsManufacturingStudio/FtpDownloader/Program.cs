437d6407e9b52f1849eb778ea77a82bcc2cb3e95
Rally Wang <v-rawang@microsoft.com>
2018-03-08 23:01:21 +0800

    FtpDownloader console app added; PowerShellFtpClient optimized with fluent ftp downloading.

---
+++ MFGExpress/WindowsManufacturingStudio/FtpDownloader/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using FluentFTP;
+
+namespace FtpDownloader
+{
+    class Program
+    {
+        const string ParamName_Host = "HOST";
+        const string ParamName_Port = "PORT";
+        const string ParamName_UserName = "USERNAME";
+        const string ParamName_Password = "PASSWORD";
+        const string ParamName_RemoteFile = "REMOTE";
+        const string ParamName_LocalFile = "LOCAL";
+        const string Pair_Separator = "=";
+        static void Main(string[] args)
+        {
+            parseArguments(args);
+
+            string host = ftpParameters[ParamName_Host];
+            int port = int.Parse(ftpParameters[ParamName_Port]);
+            string userName = ftpParameters[ParamName_UserName];
+            string password = ftpParameters[ParamName_Password];
+            string remoteFile = ftpParameters[ParamName_RemoteFile];
+            string localFile = ftpParameters[ParamName_LocalFile];
+
+            doFtpDownload(host, port, userName, password, remoteFile, localFile);
+
+            //Console.Read();
+        }
+
+        static Dictionary<string, string> ftpParameters;
+
+        static void parseArguments(string[] args)
+        {
+            if ((args != null) && (args.Length > 0))
+            {
+                string[] pair;
+                string key, value;
+                ftpParameters = new Dictionary<string, string>();
+
+                foreach (string arg in args)
+                {
+                    pair = arg.Split(new string[] { Pair_Separator},  StringSplitOptions.RemoveEmptyEntries);
+
+                    if ((pair != null) && (pair.Length > 1))
+                    {
+                        key = pair[0].Substring(1).ToUpper();
+                        value = pair[1];
+
+                        if (!ftpParameters.ContainsKey(key))
+                        {
+                            ftpParameters.Add(key, value);
+                        }
+                        else
+                        {
+                            ftpParameters[key] = value;
+                        }               
+                    }
+                }
+            }
+        }
+
+        static async void doFtpDownload(string host, int port, string userName, string password, string remoteFile, string localFile)
+        {
+            FtpClient client = new FtpClient(host);
+
+            client.Host = host; //fileInfo.Url;
+            client.Port = port; //21;
+            client.RetryAttempts = 5;
+            client.DataConnectionReadTimeout = 30;
+            client.DataConnectionConnectTimeout = 30;
+            client.ConnectTimeout = 30;
+            //client.BulkListing = false;
+            //client.SocketKeepAlive = true;
+
+            client.Credentials = new NetworkCredential(userName, password);
+            client.DataConnectionType = FtpDataConnectionType.AutoPassive; //FtpDataConnectionType.AutoActive;
+
+            client.Connect();
+
+            bool result = false;
+
+            Console.WriteLine(String.Format("Downloading file from \"{0}\" to \"{1}\"...please wait...", host, localFile));
+
+            using (FileStream stream = new FileStream(localFile, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {  
+                result = await client.DownloadAsync(stream, remoteFile);
+
+                Console.WriteLine(result);
+
+                while (!result)
+                {
+
+                }
+            }
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
--- MFGExpress/WindowsManufacturingStudio/PowerShellFTPClient/GetFTPFileCommand.cs
+++ MFGExpress/WindowsManufacturingStudio/PowerShellFTPClient/GetFTPFileCommand.cs
@@ -30,41 +30,69 @@ namespace PowerShellFTPClient
 
         [Parameter(Position = 5, Mandatory = true, HelpMessage = "The name of the file to be saved in the local place.")]
         public string LocalName { get; set; }
+
+        [Parameter(Position = 6, Mandatory = false, HelpMessage = "The length of time in milliseconds the data channel should wait for the server to send data.")]
+        public int DataReadTimeout { get; set; }
+
+        [Parameter(Position = 7, Mandatory = false, HelpMessage = "The length of time in milliseconds for a data connection to be established before giving up.")]
+        public int DataConnectTimeout { get; set; }
+
+        [Parameter(Position = 8, Mandatory = false, HelpMessage = "The length of time in milliseconds to wait for a data connection to be established before giving up.")]
+        public int ConnectTimeout { get; set; }
+
+        [Parameter(Position = 9, Mandatory = false, HelpMessage = "The retry attemps allowed when a verification failure occures during download.")]
+        public int RetryAttempts { get; set; }
         protected override void ProcessRecord()
         {
-            FtpClient client = new FtpClient(this.Host);
-
-            client.Host = this.Host; 
-            client.Port = this.Port; 
-            client.RetryAttempts = 6;
-            client.DataConnectionReadTimeout = 30;
-            client.DataConnectionConnectTimeout = 30;
-            client.ConnectTimeout = 30;
-            //client.BulkListing = false;
-            //client.SocketKeepAlive = true;
-
-            if (!String.IsNullOrEmpty(this.UserName))
+            try
             {
-                client.Credentials = new NetworkCredential(this.UserName, this.Password);
-            }          
+                FtpClient client = new FtpClient(this.Host);
 
-            client.DataConnectionType = FtpDataConnectionType.AutoPassive; //FtpDataConnectionType.AutoActive;
+                client.Host = this.Host;
+                client.Port = this.Port;
+                client.RetryAttempts = this.RetryAttempts > 0 ? this.RetryAttempts : 6;
+                client.DataConnectionReadTimeout = this.DataReadTimeout > 0 ? this.DataReadTimeout : 30;
+                client.DataConnectionConnectTimeout = this.DataConnectTimeout > 0 ? this.DataConnectTimeout : 30;
+                client.ConnectTimeout = this.ConnectTimeout > 0 ? this.ConnectTimeout : 30;
+                client.BulkListing = false;
+                client.SocketKeepAlive = true;
 
-            client.Connect();
+                if (!String.IsNullOrEmpty(this.UserName))
+                {
+                    client.Credentials = new NetworkCredential(this.UserName, this.Password);
+                }
 
-            int activityID = new Random().Next();
+                client.DataConnectionType = FtpDataConnectionType.AutoPassive; //FtpDataConnectionType.AutoActive;
 
-            using (FileStream stream = new FileStream(this.LocalName, FileMode.Create, FileAccess.Write, FileShare.Write))
-            {
+                client.Connect();
+
+                int activityID = new Random().Next();
+
                 this.WriteObject(String.Format("Downloading file from \"{0}\" to \"{1}\"...please wait...", this.Host, this.LocalName));
 
                 this.WriteProgress(new ProgressRecord(activityID, "Downloading...", String.Format("Downloading file from \"{0}\" to \"{1}\"...please wait...", this.Host, this.LocalName)) { RecordType = ProgressRecordType.Processing, PercentComplete = -1 });
 
-                client.Download(stream, this.RemoteName);
+                bool result = false;
 
-                this.WriteObject("Done.");
+                //using (FileStream stream = new FileStream(this.LocalName, FileMode.Create, FileAccess.Write, FileShare.Write))
+                //{
+                //    result = client.Download(stream, this.RemoteName);
+                //}
+
+                result = client.DownloadFile(this.LocalName, this.RemoteName, true, FtpVerify.Retry);
+
+                if (result){
+                    this.WriteObject("Done.");
+                }else{
+                    this.WriteObject("Error!");
+                }
 
                 this.WriteProgress(new ProgressRecord(activityID, "Completed", "Done.") { RecordType = ProgressRecordType.Completed, PercentComplete = 100 });
+            }
+            catch (Exception ex)
+            {
+                this.WriteWarning(ex.ToString());
+                this.WriteWarning(ex.InnerException.ToString());
             }
         }
     }
