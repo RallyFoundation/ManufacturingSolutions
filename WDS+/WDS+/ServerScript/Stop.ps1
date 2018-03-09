633c7e7959e3c276a119daf650562e05dac74809
Rally Wang <v-rawang@microsoft.com>
2018-03-09 11:39:12 +0800

    Exe for Click to Stop provided.

---
+++ WDS+/WDS+/ServerScript/Stop.ps1
@@ -0,0 +1,13 @@
+Get-Process *node*;
+Stop-Process -Name node;
+Get-Process *node*;
+
+Get-Process *python*;
+Stop-Process -Name python;
+Get-Process *python*;
+
+Get-Process *nginx*;
+Stop-Process -Name nginx;
+Get-Process *nginx*;
+
+#exit;
