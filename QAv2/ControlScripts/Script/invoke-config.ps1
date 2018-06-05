5e1b5889de43eda31ff2662acb77df45a1e0fa06
Rally Wang <v-rawang@microsoft.com>
2018-06-05 12:45:50 +0800

    supper run for starting config added; script for launching webview plus with config ui added; interface IMapper added in Core; interface IReducer added in Core.

---
+++ QAv2/ControlScripts/Script/invoke-config.ps1
@@ -0,0 +1,56 @@
+param([System.String]$TransactionID, [System.String]$RootDir, [System.String]$ServiceUrl, [System.Boolean]$ByPassUI = 0, [System.Boolean]$StayInHost = 0, [ref]$OutResult);
+
+$ErrorActionPreference = "Stop";
+
+#Checking PowerShell version and CLR (.NET Framework) version:
+$PowerShellVersionInfo = $PSVersionTable;
+
+$PowerShellVersionInfo;
+
+if([System.String]::IsNullOrEmpty($RootDir) -eq $true)
+{
+   $RootDir = Split-Path -parent $MyInvocation.MyCommand.Definition;
+
+   if($RootDir.EndsWith("\") -eq $true)
+   {
+      $RootDir = $RootDir.Substring(0, ($RootDir.Length -1));
+   }
+
+   if($RootDir.ToLower().EndsWith("\script") -eq $true)
+   {
+      $RootDir = $RootDir.Substring(0, ($RootDir.ToLower().LastIndexOf("\script")));
+   }
+}
+
+if($RootDir.EndsWith("\") -eq $true)
+{
+  $RootDir = $RootDir.Substring(0, ($RootDir.Length -1));
+}
+
+$LogPath = $RootDir +  "\Log";
+if([System.IO.Directory]::Exists($LogPath) -eq $false)
+{
+    [System.IO.Directory]::CreateDirectory($LogPath);
+	Start-Sleep -Milliseconds 1000;
+}
+
+$OutputPath = $RootDir +  "\Output";
+if([System.IO.Directory]::Exists($OutputPath) -eq $false)
+{
+    [System.IO.Directory]::CreateDirectory($OutputPath);
+    Start-Sleep -Milliseconds 1000;
+}
+
+$InputPath = $RootDir +  "\Input";
+if([System.IO.Directory]::Exists($InputPath) -eq $false)
+{
+    [System.IO.Directory]::CreateDirectory($InputPath);
+	Start-Sleep -Milliseconds 1000;
+}
+
+$ConfigUIPath = ($RootDir + "\Module\UI\Views\RuleConfig.html");
+$WebViewPlusPath = ($RootDir + "\Module\UI\WebViewPlus.exe");
+
+Start-Process -FilePath $WebViewPlusPath -ArgumentList @($ConfigUIPath) -Wait -NoNewWindow;
+
+$Host.SetShouldExit(1);
