ebde083f91f8ddaf3532755663c65dfaec20f320
Rally Wang <v-rawang@microsoft.com>
2018-07-02 14:07:23 +0800

    Revert "front-end functoin for checking field rule value against system rule settings added."
    This reverts commit bd45f91b832a933d11f550ac1d79b9a66d30375f.

--- WindowsManufacturingCloud/WindowsManufacturingCloud/Program.cs
+++ WindowsManufacturingCloud/WindowsManufacturingCloud/Program.cs
@@ -7,8 +7,6 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 
 namespace WindowsManufacturingCloud
 {
@@ -19,30 +17,9 @@ namespace WindowsManufacturingCloud
             BuildWebHost(args).Run();
         }
 
-        //public static IWebHost BuildWebHost(string[] args) =>
-        //    WebHost.CreateDefaultBuilder(args)
-        //        .UseStartup<Startup>()
-        //        //.UseUrls("http://*:5000")
-        //        .Build();
-
-        public static IWebHost BuildWebHost(string[] args)
-        {
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("hosting.json", optional: true)
-                .AddCommandLine(args)
-                .Build();
-
-            return WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:5000")
-                .UseConfiguration(config)
-                .Configure(app =>
-                {
-                    app.Run(context =>
-                        context.Response.WriteAsync("Welcome to Windows Manufacturing Cloud!"));
-                })
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-        }
     }
 }
