074fd6426357ca35b8cabe9fdd2a85d7d0a7fb3b
Rally Wang <v-rawang@microsoft.com>
2019-01-14 11:40:21 +0800

    Initial commit for vamt.js.

--- MFGSuite/OA3Agent/OA3Agent/hqa-job-executor.js
+++ MFGSuite/OA3Agent/OA3Agent/hqa-job-executor.js
@@ -11,6 +11,7 @@ var assert = require('assert');
 //var async = require("async");
 var path = require("path");
 var uuidv1 = require("uuid/v1");
+var childProcess = require("child_process");
 
 var cluster = require('cluster');
 var os = require('os');
@@ -198,7 +199,22 @@ app.post("/hqa/offline/batch/", function (req, res) {
 
     var hqaOfflineScript = hqaEntry;
 
-    ps.addCommand(hqaOfflineScript, [("ReportFileDir " + reportFileDir), ("BatchID " + transactionId), ("RootDir " + hqaHome)]);
+    //childProcess.execFile("PowerShell.exe", ["-ExecutionPolicy ByPass", "-NoExit", ("-File \"" + hqaOfflineScript + "\""), ("-ReportFileDir " + reportFileDir), ("-BatchID " + transactionId), ("-RootDir " + hqaHome)], { detached: true, stdio: ['ignore', 'ignore', 'ignore'] });
+
+    //childProcess.exec("PowerShell.exe", ["-ExecutionPolicy ByPass", ("-File " + hqaOfflineScript), ("-ReportFileDir " + reportFileDir), ("-BatchID " + transactionId), ("-RootDir " + hqaHome)], (error, stdout, stderr) => {
+    //    if (error) {
+    //        console.log(error);
+    //    }
+
+    //    if (stderr) {
+    //        console.log(stderr);
+    //    }
+
+    //    console.log(stdout);
+    //});
+
+
+    ps.addCommand(hqaOfflineScript, [("RootDir \"" + hqaHome + "\""), ("ReportFileDir \"" + reportFileDir + "\""), ("BatchID \"" + transactionId + "\"")]);
     ps.invoke().then(output => {
             console.log(output);
         }).catch(err => {
---
+++ MFGSuite/OA3Agent/OA3Agent/tcp.js
@@ -0,0 +1,29 @@
+﻿var net = require('net');
+ 
+// Configuration parameters
+var HOST = 'localhost';
+var PORT = 1234;
+ 
+// Create Server instance 
+var server = net.createServer(onClientConnected);  
+ 
+server.listen(PORT, HOST, function() {  
+  console.log('server listening on %j', server.address());
+});
+ 
+function onClientConnected(sock) {  
+  var remoteAddress = sock.remoteAddress + ':' + sock.remotePort;
+  console.log('new client connected: %s', remoteAddress);
+ 
+  sock.on('data', function(data) {
+    console.log('%s Says: %s', remoteAddress, data);
+    sock.write(data);
+    sock.write(' exit');
+  });
+  sock.on('close',  function () {
+    console.log('connection from %s closed', remoteAddress);
+  });
+  sock.on('error', function (err) {
+    console.log('Connection %s error: %s', remoteAddress, err.message);
+  });
+};
